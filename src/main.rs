34a4c9d28f42ccacc2d085af66065f530f5509ae
Kevin Exton <068ant@gmail.com>
2018-01-14 20:12:17 +1100

    started working on this

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = env::args();
+    arguments.next(); 
+
+    let text = match arguments.next() {
+        Some(arg) => arg,
+        None => String::from(""),
+    };
+ 
+    for character in text.as_str().chars(){
+        println!("Match to vowels in the english language and map to a hash table");
+        println!("{}", character);
+    }
+//    println!("{}", text);
 }
